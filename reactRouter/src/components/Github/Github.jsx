a4e967692cdb202a2f14235aca7ad8cc093dd22e
Paritosh <paritoshsharma2020@gmail.com>
2025-11-07 23:17:46 +0530

    Added routing using React Router

---
+++ reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+
+function Github() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/Paritosh-sharma1")
+      .then((response) => response.json())
+      .then((userData) => {
+        console.log(userData);
+        setData(userData);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div className="text-center m-4 bg-grey-600 text-white p-4 text-3xl">
+      Github Followers: {data.followers}
+      <img
+        className="text-center"
+        src={data.avatar_url}
+        alt="Git picture"
+        width={300}
+      />
+    </div>
+  );
+}
+
+export default Github;
